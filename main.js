a558aad30d0e2b249037b270d83da1ff41932fe4
rmxaotoi93 <47957428+rmxaotoi93@users.noreply.github.com>
2020-05-19 21:42:17 +0700

    Update main.js

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ let updateList = () =>{
     let html ="";
     for(i = 0; i< masterTodoList.length; i++){
         html += `<li> <input type="checkbox" onlick="check(${i})">${masterTodoList[i].text} 
-        ${masterTodoList[i].isDone} <button href='#' onclick='remove(${i})'>Del</button> <a href='#' id="done" onclick='toggle(${i})'>Mark done</a> </li>\n`;
+        ${masterTodoList[i].isDone} <a href='#' onclick='remove(${i})'>Del</a> <a href='#' id="done" onclick='toggle(${i})'>Mark done</a> </li>\n`;
     }
     document.getElementById('resultArea').innerHTML = html
 }
